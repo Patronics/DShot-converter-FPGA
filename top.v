db667644ac628cf3043f55f67a59d60cff1fe9a4
Patrick Leiser <patrick27leiser@yahoo.com>
2024-08-27 02:33:11 -0700

    configure FPGA for full 8-channel motor input handling

--- top.v
+++ top.v
@@ -2,11 +2,23 @@
 module top (
     input CLK,    // 16MHz clock
     output LED,   // User/boot LED next to power LED
-    input PIN_12,
-    input PIN_13,
-    output PIN_14,
-    inout PIN_16,
-    inout PIN_17,
+
+    input PIN_3,  //motor 8
+    input PIN_4,  //motor 7
+    input PIN_5,  //motor 6
+    input PIN_6,  //motor 5
+
+    //pins 7, 8, 9 are unused
+
+    input PIN_10,  //motor 4
+    input PIN_11,  //motor 3
+    input PIN_12,  //motor 2
+    input PIN_13,  //motor 1
+
+    inout PIN_14,  //i2c scl
+    inout PIN_15,  //i2c sda
+
+    //pins 16-24 are unused
 
     output USBPU  // USB pull-up resistor
 );
@@ -42,7 +54,12 @@ module top (
     wire pwmOut1Pin;
     wire [7:0] targetSpeed1;
     wire [7:0] targetSpeed2;
-
+    wire [7:0] targetSpeed3;
+    wire [7:0] targetSpeed4;
+    wire [7:0] targetSpeed5;
+    wire [7:0] targetSpeed6;
+    wire [7:0] targetSpeed7;
+    wire [7:0] targetSpeed8;
 
     speedhandler speedHandler1(
         .clk(CLK),
@@ -56,12 +73,48 @@ module top (
         .outputSpeed(targetSpeed2)
     );
 
-    pwmout testPwmOut(
+    speedhandler speedHandler3(
+        .clk(CLK),
+        .dshotPin(PIN_11),
+        .outputSpeed(targetSpeed3)
+    );
+
+    speedhandler speedHandler4(
+        .clk(CLK),
+        .dshotPin(PIN_10),
+        .outputSpeed(targetSpeed4)
+    );
+
+    speedhandler speedHandler5(
+        .clk(CLK),
+        .dshotPin(PIN_6),
+        .outputSpeed(targetSpeed5)
+    );
+
+    speedhandler speedHandler6(
+        .clk(CLK),
+        .dshotPin(PIN_5),
+        .outputSpeed(targetSpeed6)
+    );
+
+    speedhandler speedHandler7(
+        .clk(CLK),
+        .dshotPin(PIN_4),
+        .outputSpeed(targetSpeed7)
+    );
+
+    speedhandler speedHandler8(
+        .clk(CLK),
+        .dshotPin(PIN_3),
+        .outputSpeed(targetSpeed8)
+    );
+
+    /*pwmout testPwmOut(
         .clk(CLK),
         .enable(1'b1),
         .targetSpeed(targetSpeed1),
         .pwmPin(PIN_14)
-    );
+    );*/
 
     //i2c handling:
     wire [63:0] targetSpeedFlat;
@@ -71,13 +124,13 @@ module top (
     assign targetSpeedFlat[63:56] = targetSpeed1;
     //assign targetSpeedFlat[55:0] = 56'd0;
 
-    assign targetSpeedFlat[55:48] = targetSpeed1;
-    assign targetSpeedFlat[47:40] = targetSpeed2;
-    assign targetSpeedFlat[39:32] = targetSpeed1;
-    assign targetSpeedFlat[31:24] = targetSpeed1;
-    assign targetSpeedFlat[23:16] = targetSpeed1;
-    assign targetSpeedFlat[15:8] = targetSpeed1;
-    assign targetSpeedFlat[7:0] = targetSpeed1;
+    assign targetSpeedFlat[55:48] = targetSpeed2;
+    assign targetSpeedFlat[47:40] = targetSpeed3;
+    assign targetSpeedFlat[39:32] = targetSpeed4;
+    assign targetSpeedFlat[31:24] = targetSpeed5;
+    assign targetSpeedFlat[23:16] = targetSpeed6;
+    assign targetSpeedFlat[15:8] = targetSpeed7;
+    assign targetSpeedFlat[7:0] = targetSpeed8;
 
 
 
@@ -104,7 +157,7 @@ module top (
         .PULLUP(1'b 0)
     ) scl
     (
-        .PACKAGE_PIN(PIN_16),
+        .PACKAGE_PIN(PIN_14),
         .OUTPUT_ENABLE(!scl_t),
         .D_OUT_0(scl_o),
         .D_IN_0(scl_i)
@@ -115,7 +168,7 @@ module top (
         .PULLUP(1'b 0)
     ) sda
     (
-        .PACKAGE_PIN(PIN_17),
+        .PACKAGE_PIN(PIN_15),
         .OUTPUT_ENABLE(!sda_t),
         .D_OUT_0(sda_o),
         .D_IN_0(sda_i)
@@ -125,9 +178,9 @@ module top (
     //assign sda_i = PIN_17;
     //assign PIN_17 = sda_t ? 1'bz : sda_o; //sda_pin
 
+    assign LED = |targetSpeedFlat; //or reduction, if any bit set, set LED
 
-
-    assign LED = PIN_14;
+    //assign LED = PIN_14;
     //assign PIN_15 = clockOut;
 
 endmodule
